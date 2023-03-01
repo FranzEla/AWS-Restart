0aec2e6397609b7a396c5d42373f6112a8abcc39
Paula Hoha <hohapaula@gmail.com>
2023-03-01 21:28:44 +0100

    Added new line

--- numeric-data.py
+++ numeric-data.py
@@ -3,7 +3,7 @@ print("Python has three numeric types: int, float, and complex\n")
 myValue=1
 print(myValue)
 print(type(myValue))
-print(str(myValue) + " is of the data type " + str(type(myValue)))
+print(str(myValue) + " is of the data type " + str(type(myValue))"\n")
 print("")
 
 myValue=3.14
