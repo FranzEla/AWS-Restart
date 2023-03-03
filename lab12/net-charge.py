e8b00040d386063d4cfedca2df323ad183ef92f2
Paula Hoha <hohapaula@gmail.com>
2023-03-03 13:55:57 +0100

    Added Lab12

---
+++ lab12/net-charge.py
@@ -0,0 +1,50 @@
+# Python3.11.2  
+# Coding: utf-8  
+
+# Store the human preproinsulin sequence in a variable called preproinsulin:  
+preproInsulin = "malwmrllpllallalwgpdpaaafvnqhlcgshlvealylvcgergffytpktrreaedlqvgqvelgggpgagslqplalegslqkrgiveqcctsicslyqlenycn"
+
+# Store the remaining sequence elements of human insulin in variables:  
+lsInsulin = "malwmrllpllallalwgpdpaaa"  
+bInsulin = "fvnqhlcgshlvealylvcgergffytpkt"  
+aInsulin = "giveqcctsicslyqlenycn"  
+cInsulin = "rreaedlqvgqvelgggpgagslqplalegslqkr"  
+insulin = bInsulin + aInsulin
+print("This is the insulin sequence:", insulin)
+# Create a new empty dictionary
+pKR = {}
+
+# Populate the dictionary:
+pKR = {
+    'y': 10.07,
+    'c': 8.18,
+    'k': 10.53,
+    'h': 6.00,
+    'r': 12.48,
+    'd': 3.65,
+    'e': 4.25
+    }
+
+# print(pKR)
+# insulin = insulin.upper()
+# count_insulin = float(insulin.count("Y"))
+# print("There are {} Y amino acids".format(count_insulin))
+
+seqCount = ({x: float(insulin.count(x)) for x in ['y','c','k','h','r','d','e']})
+print(seqCount)
+
+pH = 0
+while (pH <= 14):
+    netCharge = (
+    +(sum({x: ((seqCount[x]*(10**pKR[x]))/((10**pH)+(10**pKR[x]))) \
+    for x in ['k','h','r']}.values()))
+    -(sum({x: ((seqCount[x]*(10**pH))/((10**pH)+(10**pKR[x]))) \
+    for x in ['y','c','d','e']}.values())))
+    print('{0:.2f}'.format(pH), netCharge)
+    pH +=1
+
+print("-------------Understanding string formatting syntax-------------------")
+x = 10.34568789
+y = 3.14159
+formatted_string = "The value of x is {0:.2f}, and the value of y is {1:.2f}".format(x, y)
+print(formatted_string)
